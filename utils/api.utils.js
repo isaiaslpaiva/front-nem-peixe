acfa578258f3bc38164548f0c560535c4798aef4
Isaias Paiva <isaiaslopespaiva@gmail.com>
2022-04-30 07:52:36 -0300

    criacao do api utils

---
+++ utils/api.utils.js
@@ -0,0 +1,104 @@
+import axios from 'axios';
+
+class Api {
+  constructor() {
+    this.api = axios.create({
+      baseURL: "http://localhost:8001"
+    })
+
+    this.api.interceptors.request.use(
+      (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+          config.headers = {
+            'Authorization': `Bearer ${token}`
+          }
+        }
+        return config;
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+
+    this.api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response.status === 401) {
+          localStorage.removeItem('token');
+          localStorage.removeItem('user');
+        }
+        throw error;
+      }
+    )
+  }
+
+  addProduct = async (title) => {
+    try {
+      const { data } = await this.api.post('/products', { title });
+      return data;
+    } catch (error) {
+      throw error.response;
+    }
+  }
+
+  getProducts = async () => {
+    try {
+      const { data } = await this.api.get('/products')
+      return data;
+    } catch (error) {
+      throw error.response;
+    }
+  }
+
+  updateProduct = async (id, product) => {
+    try {
+      await this.api.put(`/products/${id}`, product);
+    } catch (error) {
+      throw error.response;
+    }
+  }
+
+  deleteProduct = async (id) => {
+    try {
+      await this.api.delete(`/products/${id}`)
+    } catch (error) {
+      throw error.response;
+    }
+  }
+
+  login = async (user) => {
+    try {
+      const { data } = await this.api.post('/auth/login', user);
+      localStorage.setItem('token', data.token)
+      localStorage.setItem('user', JSON.stringify(data.user))
+      return data;
+    } catch (error) {
+      throw error.response;
+    }
+  }
+
+  getProfile = async () => {
+    try {
+      const {data} = await this.api.get('/user');
+      return data
+    } catch (error) {
+      throw error.response;
+    }
+  }
+
+  updateImage = async (file) => {
+    try {
+      const imgData = new FormData();
+      imgData.append('image', file);
+
+      const {data} = await this.api.put('/user/image', imgData);
+      return data;
+    } catch (error) {
+      throw error.response;
+    }
+  }
+
+}
+
+export default new Api();
